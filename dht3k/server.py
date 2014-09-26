bafce1bb2d06bfd85e94dd2b0f5c1dc907d50fe0
Jean-Louis Fuchs <ganwell@fangorn.ch>
2014-09-26 20:50:10 +0200

    True/False typo

--- dht3k/server.py
+++ dht3k/server.py
@@ -170,7 +170,7 @@ class DHTServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
                 self.socket.setsockopt(
                     socket.IPPROTO_IPV6,
                     socket.IPV6_V6ONLY,
-                    False
+                    True,
                 )
             except socket.error:
                 pass
